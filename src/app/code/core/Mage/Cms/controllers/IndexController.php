0b30d2179f32bf0e0eba14c4d518eade62273ed8
lucian.toza <ltoza@optaros.com>
2018-07-17 11:14:31 +0300

    Example of rewriting a block.

--- src/app/code/core/Mage/Cms/controllers/IndexController.php
+++ src/app/code/core/Mage/Cms/controllers/IndexController.php
@@ -52,8 +52,10 @@ class Mage_Cms_IndexController extends Mage_Core_Controller_Front_Action
 //            Mage::app()->getLayout()->createBlock("core/template")
 //            get_class(Mage::getModel("sales/quote_address"))
 //            Mage::getModel("sales/quote_address")
-            get_class(Mage::getResourceModel("first/page")) . ":: " .
-            get_class(Mage::getResourceModel("cms/page"))
+//            get_class(Mage::getResourceModel("first/page"))
+//            . ":: " .get_class(Mage::getResourceModel("cms/page"))
+            get_class(Mage::getBlockSingleton("checkout/onepage_success"))
+            . "::" . get_class(Mage::getBlockSingleton("first/onepage_success"))
         );
 
         exit;
---
+++ src/app/code/local/First/Module/Block/Onepage/Success.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucian.toza
+ * Date: 17/07/2018
+ * Time: 11:07
+ */
+
+class First_Module_Block_Onepage_Success extends Mage_Checkout_Block_Onepage_Success
+{
+
+}
