1646778192403430a2790a1960b0fc6dcc764d8e
lucian.toza <ltoza@optaros.com>
2018-07-17 11:24:58 +0300

    Example of rewriting a helper.

--- src/app/code/core/Mage/Cms/controllers/IndexController.php
+++ src/app/code/core/Mage/Cms/controllers/IndexController.php
@@ -54,8 +54,10 @@ class Mage_Cms_IndexController extends Mage_Core_Controller_Front_Action
 //            Mage::getModel("sales/quote_address")
 //            get_class(Mage::getResourceModel("first/page"))
 //            . ":: " .get_class(Mage::getResourceModel("cms/page"))
-            get_class(Mage::getBlockSingleton("checkout/onepage_success"))
-            . "::" . get_class(Mage::getBlockSingleton("first/onepage_success"))
+//            get_class(Mage::getBlockSingleton("checkout/onepage_success"))
+//            . "::" . get_class(Mage::getBlockSingleton("first/onepage_success"))
+            get_class(Mage::helper("checkout/data"))
+            . "::" . get_class(Mage::helper("first/data"))
         );
 
         exit;
---
+++ src/app/code/local/First/Module/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucian.toza
+ * Date: 17/07/2018
+ * Time: 11:22
+ */
+class First_Module_Helper_Data extends Mage_Checkout_Helper_Data
+{
+
+}
