2870e65f375ff447815bbd5bf1018ff83cd28cbd
lucian.toza <ltoza@optaros.com>
2018-07-20 11:59:45 +0300

    Added a front controller (in a new module).

---
+++ src/app/code/local/Second/Module/controllers/PastaController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucian.toza
+ * Date: 20/07/2018
+ * Time: 11:53
+ */
+
+class Second_Module_PastaController extends Mage_Core_Controller_Front_Action
+{
+    public function sleepAction()
+    {
+        echo "Sleep!";
+    }
+}
