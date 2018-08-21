aa392d3b43db7fe76c4080aa61a12381640d77cf
lucian.toza <ltoza@optaros.com>
2018-08-21 19:50:11 +0300

    Add a template in the default theme and use a core block for display.

--- src/app/code/local/Forth/Blocks/controllers/IndexController.php
+++ src/app/code/local/Forth/Blocks/controllers/IndexController.php
@@ -6,7 +6,22 @@ class Forth_Blocks_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction()
     {
         $layout = $this->getLayout();
-        $blockHtml = $layout->createBlock("forth_blocks/hello_world")->toHtml();
+        $blockHtml = $layout
+            ->createBlock("forth_blocks/hello_world")
+            ->toHtml();
+
         $this->getResponse()->setBody($blockHtml);
+    }
+
+    public function templateAction()
+    {
+        $layout = $this->getLayout();
+        $block = $layout
+            ->createBlock("core/template")
+            ->setTemplate("forth_blocks/some/random.phtml") // the function applies to Mage_Core_Block_Template children
+        ;
+
+        var_dump($block->getTemplateFile());
+        $this->getResponse()->setBody($block->toHtml());
     }
 }
