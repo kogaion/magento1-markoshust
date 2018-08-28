0c338811bffcb8f1736474b105cd543c0983f550
lucian.toza <ltoza@optaros.com>
2018-08-28 18:16:16 +0300

    Add model, resource model and collection.

---
+++ src/app/code/local/Training/Animal/Model/Animal.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Training_Animal_Model_Animal extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init("training/animal");
+        return parent::_construct();
+    }
+}
---
+++ src/app/code/local/Training/Animal/Model/Entity/Animal.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Training_Animal_Model_Entity_Animal extends Mage_Core_Model_Mysql4_Abstract
+{
+    /**
+     * Resource initialization
+     */
+    protected function _construct()
+    {
+        $this->_init("training/animal", "animal_id");
+        return parent::_construct();
+    }
+}
