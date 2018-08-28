de6b093dbb5379f215f6e9c4a1cf3e815c086c30
lucian.toza <ltoza@optaros.com>
2018-08-28 13:16:03 +0300

    Added a custom layout handle and presented it for a specific controller action.

--- src/app/code/local/Forth/Layout/controllers/IndexController.php
+++ src/app/code/local/Forth/Layout/controllers/IndexController.php
@@ -8,4 +8,10 @@ class Forth_Layout_IndexController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();
         $this->renderLayout();
     }
+
+    public function handleAction()
+    {
+        $this->loadLayout("my_cool_handle");
+        $this->renderLayout();
+    }
 }
