186fcbef6d6e2934791fa7117b05bab123fab250
lucian.toza <ltoza@optaros.com>
2018-08-28 16:48:04 +0300

    Expose the list of all the stores. Simple filtering some categories + load EAV attributes.

--- src/app/code/local/Fifth/Database/controllers/IndexController.php
+++ src/app/code/local/Fifth/Database/controllers/IndexController.php
@@ -8,4 +8,34 @@ class Fifth_Database_IndexController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();
         $this->renderLayout();
     }
+
+    public function storesAction()
+    {
+        $storesCollection = Mage::getResourceModel("core/store_collection");
+        foreach ($storesCollection as $store) {
+            var_dump(get_class($store));
+            var_dump($store->getData());
+            var_dump($store->getRootCategoryId());
+            $category = Mage::getModel("catalog/category")->load($store->getRootCategoryId());
+            var_dump($category->getData());
+        }
+    }
+
+    public function categoriesAction()
+    {
+        $categories = Mage::getResourceModel("catalog/category_collection")
+            ->addFieldToFilter("level", 1) // filter
+            ->addAttributeToSelect("name") // load extra fields for EAV entities for which we do joins
+        ;
+
+        foreach ($categories as $category) {
+            var_dump($category->getId() . ', ' . $category->getName());
+            $childrenIds = explode(",", $category->getChildren());
+            foreach ($childrenIds as $childId) {
+                $childCategory = Mage::getModel("catalog/category")
+                    ->load($childId);
+                Zend_Debug::dump($childCategory->getData());
+            }
+        }
+    }
 }
