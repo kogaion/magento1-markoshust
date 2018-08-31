eb910f02c954371f543b7b06c6c8434102317a12
lucian.toza <ltoza@optaros.com>
2018-08-31 15:02:17 +0300

    Added an adminhtml system configuration for the module & ACL for it. Display it in the backend.

---
+++ src/app/code/local/Training/Animal/Model/System/Config/Source/Show.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Training_Animal_Model_System_Config_Source_Show
+{
+    public function toOptionArray()
+    {
+        return [
+            [
+                "label" => "Hide",
+                "value" => 0,
+            ],
+            [
+                "label" => "Show",
+                "value" => 1,
+            ]
+        ];
+    }
+}
